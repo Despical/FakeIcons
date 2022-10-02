daf8dcc75d65a16f939123fbea381779f87819c2
Berke Akçen <berkeakcen@gmail.com>
2022-10-02 13:20:22 +0300

    Added project files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# FakeIcons
-Fake icons on current window app made using C++
+# Fake Icons
+Show random fake Windows icons to the screen.
+
+## License
+This code is under [GPL-3.0 License](http://www.gnu.org/licenses/gpl-3.0.html)
+
+See the [LICENSE](https://github.com/Despical/FakeIcons/blob/master/LICENSE) file for required notices and attributions.
+
+# Images
+![fake_icons](./images/fake_icons.png)
---
+++ src/FakeVirus.cpp
@@ -0,0 +1,31 @@
+﻿#include <Windows.h>
+
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+
+HICON icons[] = {
+    LoadIcon(NULL, IDI_ERROR),
+    LoadIcon(NULL, IDI_WARNING),
+    LoadIcon(NULL, IDI_INFORMATION),
+    LoadIcon(NULL, IDI_QUESTION)
+};
+
+size_t s_icons = sizeof(icons);
+
+void DrawIcons() {
+    HWND hWnd = GetDesktopWindow();
+    HDC hdc = GetWindowDC(hWnd);
+
+    DrawIcon(hdc, rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT, icons[rand() % s_icons]);
+
+    ReleaseDC(hWnd, hdc);
+}
+
+int main() {
+    while (1) {
+
+        SetCursorPos(rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT);
+    }
+
+    return 0;
+}
